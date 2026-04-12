7dbb82b7be7051623ffc79fa8c4243405a4db6fb
farmanali7277-ship-it <farman.ali7277@gmail.com>
2026-04-12 17:05:38 +0530

    Updated Admin Panel

--- recommender/admin.py
+++ recommender/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import UserProfile, Prediction
 
-# Register your models here.
+
+# UserProfile Admin
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone')
+    search_fields = ('user__username', 'phone')
+
+
+# Prediction Admin
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'predicted_label',
+        'N', 'P', 'K',
+        'temperature',
+        'humidity',
+        'ph',
+        'rainfall',
+        'created_at'
+    )
+    list_filter = ('predicted_label', 'created_at')
+    search_fields = ('user__username', 'predicted_label')
