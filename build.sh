dab64733f4618827d33e8c37081992670b620208
farmanali7277-ship-it <farman.ali7277@gmail.com>
2026-04-12 15:54:41 +0530

    Auto create superuser

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User;
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
