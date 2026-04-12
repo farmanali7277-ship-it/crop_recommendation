eb960ae3b674bec779dece564b1889250cef268d
farmanali7277-ship-it <farman.ali7277@gmail.com>
2026-04-12 15:58:16 +0530

    Superuser Account Created

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 echo "from django.contrib.auth.models import User;
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
+import os;
+username=os.environ.get('DJANGO_SUPERUSER_USERNAME');
+email=os.environ.get('DJANGO_SUPERUSER_EMAIL');
+password=os.environ.get('DJANGO_SUPERUSER_PASSWORD');
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username,email,password)" | python manage.py shell
